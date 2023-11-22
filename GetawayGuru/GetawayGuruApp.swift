331ca919fc1d4e8daafd9af685bd6c418d1468b0
Niket Bansal <niket_bansal@berkeley.edu>
2023-11-21 17:04:00 -0800

    Initial Commit

---
+++ GetawayGuru/GetawayGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetawayGuruApp.swift
+//  GetawayGuru
+//
+//  Created by Niket Bansal on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GetawayGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
