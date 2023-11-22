5edd81003c41ff8e2055b80ac8fd56b8c76c358a
Niket Bansal <niket_bansal@berkeley.edu>
2023-11-21 17:56:04 -0800

    welcome screen partially done

--- GetawayGuru/ContentView.swift
+++ GetawayGuru/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let ggblue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        WelcomeView()
+    }
+}
+
+struct WelcomeView: View {
+    var body: some View {
+        let ggblue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
+        ZStack{
+            Rectangle()
+                .foregroundStyle(ggblue)
+                .frame(height: 900)
+            VStack {
+                Spacer()
+                    .frame(height:200)
+                HStack{
+                    Image("Travel symbols 1")
+                        .frame(width:100, height:100)
+                    Spacer()
+                        .frame(width: 40)
+                    Text("GetawayGuru")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                }
+                Spacer()
+                    .frame(height: 250)
+                Button(action: {
+//                    LogInView()
+                }, label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .frame(width: 175, height: 58)
+                            .foregroundStyle(.white)
+                        
+                        Text("Continue")
+                            .foregroundStyle(.black)
+                    }
+                    
+                })
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct LogInView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
     }
 }
 
