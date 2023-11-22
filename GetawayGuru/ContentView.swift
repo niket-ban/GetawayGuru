847ff58bfb09f3511049a11ae2d5edd2af0ff4e5
Niket Bansal <niket_bansal@berkeley.edu>
2023-11-21 18:16:49 -0800

    log in navigation

--- GetawayGuru/ContentView.swift
+++ GetawayGuru/ContentView.swift
@@ -16,41 +16,48 @@ struct ContentView: View {
 }
 
 struct WelcomeView: View {
+    @State private var isPresented = false
     var body: some View {
         let ggblue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
-        ZStack{
-            Rectangle()
-                .foregroundStyle(ggblue)
-                .frame(height: 900)
-            VStack {
-                Spacer()
-                    .frame(height:200)
-                HStack{
-                    Image("Travel symbols 1")
-                        .frame(width:100, height:100)
+        NavigationStack{
+            ZStack{
+                Rectangle()
+                    .foregroundStyle(ggblue)
+                    .frame(height: 900)
+                VStack {
                     Spacer()
-                        .frame(width: 40)
-                    Text("GetawayGuru")
-                        .font(.title)
-                        .foregroundStyle(.white)
-                }
-                Spacer()
-                    .frame(height: 250)
-                Button(action: {
-//                    LogInView()
-                }, label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 15.0)
-                            .frame(width: 175, height: 58)
+                        .frame(height:200)
+                    HStack{
+                        Image("Travel symbols 1")
+                            .frame(width:100, height:100)
+                        Spacer()
+                            .frame(width: 40)
+                        Text("GetawayGuru")
+                            .font(.title)
                             .foregroundStyle(.white)
-                        
-                        Text("Continue")
-                            .foregroundStyle(.black)
                     }
-                    
-                })
+                    Spacer()
+                        .frame(height: 250)
+                    Button(action: {
+                        //                    LogInView()
+                        isPresented = true
+                    }, label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 15.0)
+                                .frame(width: 175, height: 58)
+                                .foregroundStyle(.white)
+                            
+                            Text("Continue")
+                                .foregroundStyle(.black)
+                        }
+                        
+                    })
+                    .navigationDestination(isPresented: $isPresented){
+                        LogInView()
+                    }
+                }
+                .padding()
             }
-            .padding()
         }
     }
 }
